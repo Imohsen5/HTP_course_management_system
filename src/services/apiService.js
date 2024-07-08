82f9db0df40620ba37d495702df99a73db084c02
Imohsen5 <ibr_mo@hotmail.com>
2024-07-08 08:51:27 +0300

    chore: Update API endpoints in apiService.js

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,34 +1,31 @@
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
 const apiService = {
     submitApplication: async (data) => {
-        const response = await fetch(
-            "https://expert-memory-ppx6j65xj5rh5jg-5000.app.github.dev/api/applications",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            }
-        );
+        const response = await fetch(backendUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        if (!response.ok) throw new Error("Network response was not ok. 1");
         return response.json();
     },
     getApplications: async () => {
-        const response = await fetch(
-            "https://expert-memory-ppx6j65xj5rh5jg-5000.app.github.dev/api/applications"
-        );
+        const response = await fetch(backendUrl);
+        if (!response.ok) throw new Error("Network response was not ok. 2");
         return response.json();
     },
     updateApplicationStatus: async (id, status) => {
-        const response = await fetch(
-            `https://expert-memory-ppx6j65xj5rh5jg-5000.app.github.dev/api/applications/${id}/status`,
-            {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ status }),
-            }
-        );
+        const response = await fetch(`${backendUrl}/${id}/status`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ status }),
+        });
+        if (!response.ok) throw new Error("Network response was not ok. 3");
         return response.json();
     },
 };
